6063995eefec3da4c49f49c5c24ac8344f2d0070
f96u <gtfukui@gmail.com>
2025-03-13 23:25:44 +0900

    テスト: Pageコンポーネントのレンダリングを非同期に変更

--- __tests__/app/page.test.tsx
+++ __tests__/app/page.test.tsx
@@ -1,10 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import Page from '@/app/page'
 import { describe } from 'node:test';
 
 describe('Page', () => {
-  it('renders a heading', () => {
-    render(<Page />);
+  it('renders a heading', async () => {
+    await act(() => render(<Page />));
+    screen.debug();
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
 })
