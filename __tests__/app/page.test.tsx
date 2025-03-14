ec87234dd58bb6609ae70d0bc0f71fab1cc4e08a
f96u <gtfukui@gmail.com>
2025-03-14 22:30:09 +0900

    add: モックフェッチ関数を追加し、Pageコンポーネントのテストを更新

--- __tests__/app/page.test.tsx
+++ __tests__/app/page.test.tsx
@@ -1,11 +1,12 @@
 import { act, render, screen } from '@testing-library/react';
 import Page from '@/app/page'
 import { describe } from 'node:test';
+import { mockFetch } from '@/mocks/mockFetch';
 
 describe('Page', () => {
+  window.fetch = mockFetch({ data: { name: 'test' } });
   it('renders a heading', async () => {
     await act(() => render(<Page />));
-    screen.debug();
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
 })
