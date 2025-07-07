a3fccbe807abfda4794da1dc14cc40ed2e471061
f96u <gtfukui@gmail.com>
2025-07-07 09:06:23 +0900

    fix: test

--- __tests__/app/page.test.tsx
+++ __tests__/app/page.test.tsx
@@ -2,8 +2,8 @@ import { act, render, screen } from '@testing-library/react';
 import Page from '@/app/page'
 
 describe('Page', () => {
-  it('renders a button', async () => {
+  it('renders a chat widget', async () => {
     await act(() => render(<Page />));
-    expect(screen.getByText('Count Up !')).toBeInTheDocument();
+    expect(screen.getByText('チャット')).toBeInTheDocument();
   });
 })
